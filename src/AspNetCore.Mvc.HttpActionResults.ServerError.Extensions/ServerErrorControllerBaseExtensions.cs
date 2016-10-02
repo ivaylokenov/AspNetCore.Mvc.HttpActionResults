e0a255ab78f8697e52f5847440740ee5a974f05b
apxoht <ivaylo.kenov@gmail.com>
2016-10-02 16:41:52 +0300

    Implemented 501 Not Implemented status code (#1)

---
+++ src/AspNetCore.Mvc.HttpActionResults.ServerError.Extensions/ServerErrorControllerBaseExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Microsoft.AspNetCore.Mvc
+{
+    /// <summary>
+    /// Class containing server error HTTP response extensions methods for <see cref="ControllerBase"/>. 
+    /// </summary>
+    public static class ServerErrorControllerBaseExtensions
+    {
+        /// <summary>
+        /// Creates an <see cref="NotImplementedResult"/> object that produces an Not Implemented (501) response.
+        /// </summary>
+        /// <param name="controller">MVC controller instance.</param>
+        /// <returns>The created <see cref="NotImplementedResult"/> for the response.</returns>
+        public static NotImplementedResult NotImplemented(this ControllerBase controller)
+            => new NotImplementedResult();
+    }
+}
---
+++ src/AspNetCore.Mvc.HttpActionResults.ServerError/NotImplementedResult.cs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.AspNetCore.Mvc
+{
+    using Http;
+
+    /// <summary>
+    /// A <see cref="StatusCodeResult"/> that when executed will produce a
+    /// <see cref="StatusCodes.Status501NotImplemented"/> response.
+    /// </summary>
+    public class NotImplementedResult : StatusCodeResult
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotImplementedResult"/> class.
+        /// </summary>
+        public NotImplementedResult()
+            : base(StatusCodes.Status501NotImplemented)
+        {
+        }
+    }
+}
