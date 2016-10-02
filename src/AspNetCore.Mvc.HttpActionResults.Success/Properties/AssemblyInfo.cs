ee28dadff73d0c0a1162ae262ceddfd36c79fcd9
apxoht <ivaylo.kenov@gmail.com>
2016-10-02 14:35:58 +0300

    Created initial project structure

---
+++ src/AspNetCore.Mvc.HttpActionResults.Success/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AspNetCore.Mvc.HttpActionResults.Success")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("958d691d-a587-45c1-b617-0e9ead05a86a")]
