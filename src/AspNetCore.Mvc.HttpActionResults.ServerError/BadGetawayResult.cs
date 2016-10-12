301ef3d8f151380db82dcafd6f649d754b83b4bf
Emil Nik <emil.nik1002@gmail.com>
2016-10-12 18:37:57 +0300

    Fixed typo

--- src/AspNetCore.Mvc.HttpActionResults.ServerError/BadGetawayResult.cs
+++ src/AspNetCore.Mvc.HttpActionResults.ServerError/BadGetawayResult.cs
@@ -6,12 +6,12 @@
     /// A <see cref="StatusCodeResult"/> that when executed will produce a
     /// <see cref="StatusCodes.Status502BadGateway"/> response.
     /// </summary>
-    public class BadGetawayResult : StatusCodeResult
+    public class BadGatewayResult : StatusCodeResult
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="BadGetawayResult"/> class.
+        /// Initializes a new instance of the <see cref="BadGatewayResult"/> class.
         /// </summary>
-        public BadGetawayResult()
+        public BadGatewayResult()
             : base(StatusCodes.Status502BadGateway)
         {
         }
