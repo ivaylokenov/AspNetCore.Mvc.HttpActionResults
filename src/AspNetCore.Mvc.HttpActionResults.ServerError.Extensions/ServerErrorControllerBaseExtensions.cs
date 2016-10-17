a7782bdf2913ae739f232e2e1acd5749526088c7
Georgi Manov <georgimanov@gmail.com>
2016-10-17 13:16:10 +0300

    Update ServerErrorControllerBaseExtensions.cs

--- src/AspNetCore.Mvc.HttpActionResults.ServerError.Extensions/ServerErrorControllerBaseExtensions.cs
+++ src/AspNetCore.Mvc.HttpActionResults.ServerError.Extensions/ServerErrorControllerBaseExtensions.cs
@@ -1,4 +1,4 @@
-﻿namespace Microsoft.AspNetCore.Mvc
+namespace Microsoft.AspNetCore.Mvc
 {
     using System;
 
@@ -31,12 +31,31 @@
             return new ExceptionResult(exception);
         }
 
-        /// <summary>
-        /// Creates an <see cref="NotImplementedResult"/> object that produces a Not Implemented (501) response.
-        /// </summary>
-        /// <param name="controller">MVC controller instance.</param>
-        /// <returns>The created <see cref="NotImplementedResult"/> for the response.</returns>
-        public static NotImplementedResult NotImplemented(this ControllerBase controller)
+		/// <summary>
+		/// Creates an <see cref="ExceptionResult"/> object that produces an Internal Server Error (500) response.
+		/// </summary>
+		/// <param name="controller">MVC controller instance.</param>
+		/// <param name="exception">The exception to include in the error.</param>
+		/// <param name="includeErrorDetail">
+		/// <see langword="true"/> if the error should include exception messages; otherwise, <see langword="false"/>.
+		/// </param>
+		/// <returns>The created <see cref="ExceptionResult"/> for the response.</returns>
+		public static ExceptionResult InternalServerError(this ControllerBase controller, Exception exception, bool includeErrorDetail)
+		{
+			if (exception == null)
+			{
+				throw new ArgumentNullException(nameof(exception));
+			}
+
+			return new ExceptionResult(exception, includeErrorDetail);
+		}
+
+		/// <summary>
+		/// Creates an <see cref="NotImplementedResult"/> object that produces a Not Implemented (501) response.
+		/// </summary>
+		/// <param name="controller">MVC controller instance.</param>
+		/// <returns>The created <see cref="NotImplementedResult"/> for the response.</returns>
+		public static NotImplementedResult NotImplemented(this ControllerBase controller)
             => new NotImplementedResult();
 
         /// <summary>
